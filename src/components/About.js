ffb14a7b2d0564a0a0e33d4ae5bdf2ea81b9a15a
shalini <shalini.g9995@gmail.com>
2025-09-21 20:36:08 +0530

    content updated

--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,12 @@ export default function About(){
                 <img className='w-full md:w-1/2 flex' src={Aboutpic} alt='shalini-image'/>
             <div className=' w-full md:w-1/2 text-center text-2xl pb-5 md:py-20 px-10 md:pr-10 text-white'>
                 <h1 className='text-center text-3xl bg-secondary text-red-600 mb-3 font-anton-font w-[100px] mx-auto'>My Self</h1>
-                <p className='py-2'>I am a Full Stack Web developer in <a className= 'text-white hover:text-yellow-300 bg-black px-2' href='https://www.linkedin.com/company/forbes-advisor/'>Forbes Advisor</a></p>
+                <p className='py-2'>I am a Software Engineer at <a className= 'text-white hover:text-yellow-300 bg-black px-2' href='https://www.linkedin.com/company/piquota-digital-solutions'>Piquota</a></p>
                 <p>As I have selected for an Internship in Forbes Advisor Company via VIT College Campus placement. And I have learned creating wordpress website using Php, JavaScript, React Js and more.After completed my 6 months Internship I am converted to Full time Associate Software Engineer.</p><br/>
-                <p>I began my work journey here and currently serve as a Full Stack Web Developer at Forbes Advisor. As a Backend Developer I manage WordPress sites like <a className= 'text-blue-500 hover:text-yellow-300 ' href='https://www.usatoday.com/'>USA Today,</a> <a className= 'text-red-500 hover:text-yellow-300 ' href='https://edition.cnn.com/'> CNN </a> and <a className= 'text-purple-500 hover:text-yellow-300 ' href='https://www.forbes.com/advisor/in/'>Forbes Advisor</a>. My work includes developing essential Gutenberg blocks, Themes, building templates and creating plugins using PHP, JavaScript, JSX, React.js, and Tailwind CSS. I also use Git for version control.</p>
+                <p>I began my work journey here. As a Backend Developer I manage WordPress sites like <a className= 'text-blue-500 hover:text-yellow-300 ' href='https://www.usatoday.com/'>USA Today,</a> <a className= 'text-red-500 hover:text-yellow-300 ' href='https://edition.cnn.com/'> CNN </a> and <a className= 'text-purple-500 hover:text-yellow-300 ' href='https://www.forbes.com/advisor/in/'>Forbes Advisor</a>. My work includes developing essential Gutenberg blocks, Themes, building templates and creating plugins using PHP, JavaScript, JSX, React.js, and Tailwind CSS. 
+                <br/>Currently, I work in Node.js web development, designing and implementing modern backend systems and APIs in Piquota Digital Solutions.</p>
                 <br/>
-                <p>In addition to this, I am proficient in Java, Python and SQL programming languages.</p>
+                <p>To strengthen my technical foundation, I actively practice Data Structures & Algorithms in Java and prepare for opportunities with top-tier product companies.</p>
             </div>
         </section>
     );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,17 +2,18 @@
 
 import '../App.css';
 import Shalini1 from '../assets/shalini1.jpeg';
-import { FaLinkedinIn } from "react-icons/fa6";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa6";
 
 function Hero(){
     return(
         <section className='flex bg-secondary flex-col md:flex-row md:pl-20' id='hero'>
             <h1 className='w-full px-10 pt-10 pb-2 md:py-36 text-5xl justify-center bg-secondary font-anton-font'> Hello,<br/> <div className='py-5'>This is <span className='text-red-600'>Shalini</span></div>
             <p className='text-3xl py-2 font-passion-one text-green-950'> Welcome to my software development portfolio! </p>
-            <p className='text-3xl py-2 font-passion-one text-green-950'> I am a 1+ years experienced Full Stack developer </p>
+            <p className='text-3xl py-2 font-passion-one text-green-950'> I am a 2+ years experienced Full Stack developer </p>
             <p className='text-3xl py-2 font-passion-one text-green-950'>Check About me...</p>
-            <span className='flex py-3'>
-                <a className='flex hover:text-red-600' href='https://www.linkedin.com/in/shalini-g-239b7b29b/'><FaLinkedinIn/></a> 
+            <span className='flex py-3 gap-4'>
+                <a className='flex hover:text-red-600' href='https://www.linkedin.com/in/shalini-g-239b7b29b/'><FaLinkedinIn/></a>
+                <a className='flex hover:text-red-600' href='https://github.com/shaliniga'><FaGithub/></a>  
             </span>
             </h1>           
             <img className='md:w-1/2 h-1/2 mx-10 rounded-full pb-4 md:py-1' src={Shalini1} alt='shalini profile'/>
